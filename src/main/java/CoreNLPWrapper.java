ee96d1e1a6084a2f8f5983d21f10c2eb947a6788
Soundararajan Dhakshinamoorthy <dakshins@advisory.com>
2018-04-16 16:54:23 +0530

    Perform NLP Pipeline initialization as part of the web server initialization sequence. NLP PIpeline initialization takes ~ 1 minute. Increase the size of heap to 8192 MB, else loading kbp annotator will resulte in OOM because of unavailable heap memory. Make the pipeline.annotate() call thread safe and make sure we are creating an instance of NLPWrapper on each request to search. The pipeline is made static to make sure we have only one instance.

--- src/main/java/CoreNLPWrapper.java
+++ src/main/java/CoreNLPWrapper.java
@@ -1,9 +1,14 @@
-import edu.stanford.nlp.simple.*;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.util.CollectionFactory;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.logging.JavaUtilLoggingAdaptor;
+import edu.stanford.nlp.util.logging.Redwood;
+import edu.stanford.nlp.util.logging.RedwoodConfiguration;
+import edu.stanford.nlp.util.logging.RedwoodPrintStream;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 /**
@@ -12,25 +17,52 @@ import java.util.Collections;
 public class CoreNLPWrapper
 {
     final String searchText;
-    final Sentence sentence;
-    public CoreNLPWrapper(final String searchText)
+    final CoreDocument document;
+    private static StanfordCoreNLP pipeline;
+    
+    public static void initialize()
     {
-        this.searchText = searchText.replace('+',' ');
-        Collections.sort(, Collections.reverseOrder());
-        sentence = new Sentence(searchText);
+        System.out.println("Initializing NLP pipeline...");
+        RedwoodConfiguration.standard().apply();
+        pipeline = new StanfordCoreNLP(properties());
+    
+        System.out.println("Completed initializing pipeline...");
     }
     
+    public CoreNLPWrapper(final String searchText)
+    {
+        this.searchText = searchText.replace('+', ' ');
+        document = new CoreDocument(searchText);
+    }
+    
+    private static Properties properties()
+    {
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse,coref,kbp,quote");
+        props.setProperty("coref.algorithm", "neural");
+        return props;
+    }
     
     public CoreNLPHttpResponse response()
     {
+        annotate(document);
         StringBuilder builder = new StringBuilder();
         builder.append("<ul>");
-        Tree tree =  sentence.parse();
-        for(String tag : sentence.nerTags())
-        {
-            builder.append("<li>"+ tag + "</li>");
-        }
+    
+        CoreSentence sentence = document.sentences().get(0);
+    
+        String nerTags = String.join(",", sentence.nerTags());
+        builder.append("<li>" + nerTags + "</li>");
+    
+        String posTags = String.join(",", sentence.posTags());
+        builder.append("<li>" + posTags + "</li>");
+    
         builder.append("</ul>");
         return new CoreNLPHttpResponse(builder.toString(), 200);
+    }
+    
+    private synchronized void annotate(CoreDocument coreDocument)
+    {
+        pipeline.annotate(coreDocument);
     }
 }
--- src/main/java/SearchHttpServer.java
+++ src/main/java/SearchHttpServer.java
@@ -6,9 +6,11 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
 import com.sun.net.httpserver.HttpServer;
+import edu.stanford.nlp.util.logging.RedwoodConfiguration;
 
 public class SearchHttpServer
 {
+    private final int port = 9080;
     public static void main(String[] args)
     {
         try
@@ -26,9 +28,18 @@ public class SearchHttpServer
     
     private SearchHttpServer() throws  Exception
     {
-        server = HttpServer.create(new InetSocketAddress(9080), 0);
+        System.out.println("Beging server initialization...");
+        initializeRoutine();
+        System.out.println("Initialization sequence complete. Server up and running");
+        System.out.println("Browse to http://localhost:"+port);
+    }
+    
+    private void initializeRoutine() throws Exception
+    {
+        server = HttpServer.create(new InetSocketAddress(port), 0);
         server.setExecutor(Executors.newFixedThreadPool(8));
         Routes.setup(server);
+        CoreNLPWrapper.initialize();
     }
     
     private void startHttpServerAndWait() throws Exception
